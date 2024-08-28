bdc6f02e28c59ed6f1779284b4b3943725a4b1fc
migueldiascoelho <120723156+migueldiascoelho@users.noreply.github.com>
2024-08-28 15:25:49 +0100

    Changed the AWS Badge link

--- src/Certifications.js
+++ src/Certifications.js
@@ -8,7 +8,7 @@ const Certifications2 = () => {
   return (
     <div className="certification-container">
       <div className="badge-row">
-        <a href="https://www.credly.com/earner/earned/badge/895a5883-6fb2-4e21-8bbf-2461a2125a67" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.credly.com/badges/895a5883-6fb2-4e21-8bbf-2461a2125a67" target="_blank" rel="noopener noreferrer">
           <img src={awsCloud} alt="AWS Cloud Certification" className="certification-badge" />
         </a>
         <img src={blankBadge} alt="Empty Badge" className="certification-badge" />
