368d29e2e067f60734bd376fcac8ae417f42f6ca
migueldiascoelho <120723156+migueldiascoelho@users.noreply.github.com>
2024-09-05 17:09:04 +0100

    Added "Google Cybersecurity Certificate"

--- src/Certifications.js
+++ src/Certifications.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 
 import awsCloud from './graphics/aws.png';
+import cybersecurity from './graphics/cybersecurity.png';
 import blankBadge from './graphics/blankBadge.png';
 
 const Certifications2 = () => {
@@ -11,7 +12,9 @@ const Certifications2 = () => {
         <a href="https://www.credly.com/badges/895a5883-6fb2-4e21-8bbf-2461a2125a67" target="_blank" rel="noopener noreferrer">
           <img src={awsCloud} alt="AWS Cloud Certification" className="certification-badge" />
         </a>
-        <img src={blankBadge} alt="Empty Badge" className="certification-badge" />
+        <a href="https://www.credly.com/badges/45d00fea-84e4-4c05-a8ab-0fe77ee5f8ff/public_url" target="_blank" rel="noopener noreferrer">
+          <img src={cybersecurity} alt="Google Cybersecurity Certificate" className="certification-badge" />
+        </a>
         <img src={blankBadge} alt="Empty Badge" className="certification-badge" />
         <img src={blankBadge} alt="Empty Badge" className="certification-badge" />
       </div>
